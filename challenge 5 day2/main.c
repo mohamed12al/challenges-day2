f8af49b7e6562763483ebc39685a90a6aec4ba8b
Mohamed <alimohamed2simo@gmail.com>
2025-09-03 14:13:33 +0100

    test

---
+++ challenge 5 day2/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int j;
+    int n;
+    int total = 1;
+    printf("pick a number for X:");
+    scanf("%d",&j);
+    printf("pick a number for n:");
+    scanf("%d",&n);
+    if(j==0)
+    total = 1;
+    else{
+ for (int i=0 ; i < n; i++)
+ {
+    total *= j;
+ }
+}
+ printf("resault is : %d " , total);
+    return 0;
+}
+
+
+
+
+
